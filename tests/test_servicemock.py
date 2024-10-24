49988307b2f317ec7551031512e3e6d9e928c4be
ptobiasdiaz <tobias.diaz@uclm.es>
2024-10-24 20:44:05 +0200

    Fix linters

--- tests/test_servicemock.py
+++ tests/test_servicemock.py
@@ -12,20 +12,25 @@ class TestServiceMock(unittest.TestCase):
     '''Test cases to check the mock'''
 
     def test_owner_of_admin_token(self):
+        '''Test admin token'''
         self.assertEqual(Service().token_owner('token_for_admin'), 'administrator')
 
     def test_owner_of_user_token(self):
+        '''Test user token'''
         self.assertEqual(Service().token_owner('token_for_user'), 'user')
 
     def test_wrong_token(self):
+        '''Test wrong token'''
         with self.assertRaises(TokenNotFound):
             Service().token_owner('other_token')
 
     def test_roles_of_admin(self):
+        '''Test admin roles'''
         self.assertIn('admin', Service().token_roles('token_for_admin'))
         self.assertIn('user', Service().token_roles('token_for_admin'))
         self.assertEqual(len(Service().token_roles('token_for_admin')), 2)
 
     def test_roles_of_user(self):
+        '''Test user roles'''
         self.assertIn('user', Service().token_roles('token_for_user'))
         self.assertEqual(len(Service().token_roles('token_for_user')), 1)
